d1bd32f4836120b6edc4b934b7fc58a1c549fc03
ungerfall <ungerfall@gmail.com>
2026-02-17 23:52:41 +0300

    Merge stashed

--- StravaSharp.OAuth2Client/UriExtensions.cs
+++ StravaSharp.OAuth2Client/UriExtensions.cs
@@ -16,21 +16,13 @@
                 input = input.Substring(1);
             }
 
-            return input.Split('&').
-                Select(keyValuePairs =>
+            return input.Split('&')
+                .Select(static keyValuePairs =>
                 {
                     string[] keyAndValue = keyValuePairs.Split('=');
                     return new KeyValuePair<string, string>(keyAndValue[0], keyAndValue[1]);
                 })
-                .ToDictionary(x => x.Key, x => x.Value);
+                .ToDictionary(static x => x.Key, static x => x.Value);
         }
-
-        return input.Split('&').
-            Select(static keyValuePairs =>
-            {
-                string[] keyAndValue = keyValuePairs.Split('=');
-                return new KeyValuePair<string, string>(keyAndValue[0], keyAndValue[1]);
-            })
-            .ToDictionary(static x => x.Key, static x => x.Value);
     }
 }
