3f0d82fc880e3d56e8c6e97b2f8ebcdc770463fb
ungerfall <ungerfall@gmail.com>
2026-02-18 03:42:06 +0300

    Init oauth2 config

--- infographic-group/Program.cs
+++ infographic-group/Program.cs
@@ -1,4 +1,5 @@
 ﻿using StravaSharp;
+using StravaSharp.OAuth2Client;
 using StravaInfographics;
 using Microsoft.Extensions.Configuration;
 using Dumpify;
@@ -7,6 +8,12 @@ IConfigurationRoot configurationRoot = new ConfigurationBuilder()
     .AddUserSecrets<Program>()
     .Build();
 
+OAuth2ClientConfiguration config = configurationRoot
+    .GetSection(OAuth2ClientConfiguration.SectionName)
+    .Get<OAuth2ClientConfiguration>()
+    ?? throw new ArgumentNullException(nameof(configurationRoot), "Strava config section is missing.");
+StravaClient strava = new(config);
+
 string accessToken = configurationRoot["Strava:AccessToken"] ?? throw new ArgumentNullException(nameof(configurationRoot));
 Authenticator authenticator = new(accessToken);
 Client client = Client.Create(authenticator);
@@ -14,4 +21,3 @@ Client client = Client.Create(authenticator);
 IEnumerable<ActivitySummary> activities = await client.Activities.GetAthleteActivities();
 
 activities.Dump();
-
